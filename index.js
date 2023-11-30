df031051cebffff38d63915e9c1cd18cfd483bba
misha1290 <eremenko2006qwertyuiop@gmail.com>
2023-11-30 19:10:15 +0200

    1

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1
+let result = 5 + 5 + '5'
+console.log(typeof result)
+
+// 2
+let email = 'Petryshka.229@gmail.com'
+console.log(email.includes('@')) 
+console.log(email.length)
+
+// 3
+let my = 'my'
+let name = 'name'
+let is = 'is'
+console.log(`${my} ${name} ${is} Viktor`)
+
+// 4
+let userName = 'Олександро'
+let payment = 300
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
